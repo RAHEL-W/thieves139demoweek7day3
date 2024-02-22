3784ecd4feac9e031c43f9f9ca60a14a24340614
RAHEL <richassefa27@gmail.com>
2024-02-22 06:20:18 -0800

    hw

---
+++ pokemon.js
@@ -0,0 +1,34 @@
+const my_form = document.querySelector('form')
+console.log(my_form)
+
+my_form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    pokename = event.target[0].value
+    console.log(pokename)
+    pokemonData(pokename)
+})
+
+
+const pokemonData = async (pokename) => {
+    try{
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokename}`)
+      const data = await response.json()
+      console.log(data)
+      addPoke(data)
+    } catch(error) {
+      console.error(`there was an error: ${error}`)
+    }
+  }
+
+  const card = document.querySelector('#card')
+  console.log(card)
+  
+  const addPoke = (data) => {
+    console.log(data.sprites.front_default)
+    card.innerHTML = `
+       
+        <img src="${data.sprites.front_default}">
+        <p>${data.name}<p>
+        <p>${data.abilities[0]}<p>
+    `
+  }
